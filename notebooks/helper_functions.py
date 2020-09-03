2f32b6789362ff3a8b807db727ac29a51591636a
Nick Bristow <webmail@nickbristow.co.uk>
2020-09-03 10:59:03 +0100

    remove carto_type_plot add helper_functions

--- notebooks/helper_functions.py
+++ notebooks/helper_functions.py
@@ -1,4 +1,8 @@
+import pandas as pd
+import numpy as np
+
 import matplotlib.pyplot as plt
+from matplotlib.patches import Patch
 
 # colours https://github.com/gravitystorm/openstreetmap-carto/blob/master/style/landcover.mss
 # default value already set as 'other'
@@ -13,10 +17,15 @@ tag_styles = {
         't-bar': {'linecolor': '#808080'},
         'mixed_lift': {'linecolor': '#808080'},
         'platter': {'linecolor': '#808080'},
+        'pylon': {'markercolor': '#808080'},
         'rope_tow': {'linecolor': '#808080'},
+        'station': {'facecolor': 'none', 'edgecolor':'none'},
         'zip_line': {'linecolor': '#808080'},
     },
     'aeroway': {
+        # generic
+        'airtransport': {'facecolor': '#e9e7e2', 'edgecolor': '#83827e', 'markercolor': '#8461C4'},
+        #
         'runway': {'facecolor': '#bbc', 'linecolor': '#bbc', 'linewidth': 30},
     },
     'amenity': {
@@ -24,8 +33,11 @@ tag_styles = {
         'amenity-brown': {'markercolor': '#734a08'},
         'gastronomy-icon': {'markercolor': '#C77400'},
         'health-color': {'markercolor': '#BF0000'},
-        'man-made-icon':{'markercolor': '#666666'},
+        'man-made-icon': {'markercolor': '#666666'},
+        'place_of_worship': {'facecolor': '#d0d0d0', 'edgecolor': '#929292', 'markercolor': '#000000'},
+        'religious-icon': {'markercolor': '#000000'},
         'societal_amenities': {'facecolor': '#ffffe5', 'edgecolor': '#595950'},
+        'transportation-icon': {'markercolor': '#0092da'},
         # tags
         'fire_station': {'facecolor': '#F3E3DD'},
         'parking': {'facecolor': '#eeeeee', 'edgecolor': '#616161'}, # added edgecolor
@@ -55,9 +67,10 @@ tag_styles = {
         'transportation-icon':{'markercolor': '#0092da'},
         # tags
         'crossing': {'markercolor': 'none'},
-        'cycleway': {'linecolor': 'blue'},
+        'cycleway': {'linecolor': 'blue', 'linestyle': 'dashed'},
         'footway': {'linecolor': 'salmon', 'linestyle': 'dotted'},
         'giveway': {'markercolor': 'none'},
+        'living_street': {'linecolor': '#ededed'},
         'motorway': {'linecolor': '#e892a2'},
         'path': {'linecolor': 'salmon', 'linestyle': 'dotted'},
         'pedestrian': {'facecolor': '#dddde8', 'linecolor': '#dddde8'},
@@ -88,6 +101,7 @@ tag_styles = {
         'greenfield': {'facecolor': 'none'},
         'industrial': {'facecolor': '#ebdbe8', 'edgecolor': '#c6b3c3', 'linecolor': '#c6b3c3'},
         'meadow': {'facecolor': '#cdebb0'},
+        'military': {'facecolor': '#f55', 'edgecolor': '#f55'},
         'railway': {'facecolor': '#ebdbe8'},
         'residential': {'facecolor': '#e0dfdf', 'edgecolor': '#b9b9b9', 'linecolor': '#b9b9b9'},
         'retail': {'facecolor': '#ffd6d1', 'edgecolor': '#d99c95', 'linecolor': '#d99c95'},
@@ -98,7 +112,7 @@ tag_styles = {
         'common': {'facecolor': 'none'},
         'nature_reserve': {'facecolor': 'none', 'edgecolor': '#add19e'}, # added
         'marina': {'facecolor': 'none', 'edgecolor': 'blue', 'linecolor': 'blue'}, # added
-        'park': {'facecolor': '#c8facc'},
+        'park': {'facecolor': '#c8facc', 'edgecolor':'#4e614f'},
         'pitch': {'facecolor': '#aae0cb', 'edgecolor': '#6e9d8a'},
         'stadium': {'facecolor': 'none'},
         'other': {'facecolor': 'red'},
@@ -106,17 +120,20 @@ tag_styles = {
     'natural': {
         # generic
         'bare_ground': {'facecolor': '#eee5dc', 'edgecolor': 'none'},
+        'beach': {'facecolor': '#fff1ba', 'edgecolor': 'none'},
         'land-color': {'facecolor': '#f2efe9', 'edgecolor': 'none'},
         'landform-color': {'markercolor': '#d08f55'},
         # tags
         'cliff': {'facecolor': 'none', 'linecolor': 'lightgrey'}, # added
         'coastline': {'facecolor': 'none', 'edgecolor': 'none', 'linecolor': 'none'},
-        'forest': {'facecolor': '#add19e'},
+        'forest': {'facecolor': '#add19e', 'edgecolor': 'none'},
+        'glacier': {'facecolor': '#ddecec', 'edgecolor': '#9cf'},
         'heath': {'facecolor': '#d6d99f', 'edgecolor': 'none'},
-        'scrub': {'facecolor': '#c8d7ab'},
+        'sand': {'facecolor': '#f5e9c6', 'edgecolor': 'none'},
+        'scrub': {'facecolor': '#c8d7ab', 'edgecolor': 'none'},
         'tree': {'markercolor': 'green'},
         'tree_row': {'linecolor': 'green'},
-        'water': {'facecolor': '#aad3df', 'edgecolor': '#9cf', 'linecolor': '#9cf'},
+        'water': {'facecolor': '#aad3df', 'edgecolor': '#9cf', 'linecolor': '#9cf', 'markercolor': '#9cf'},
         'wetland': {'facecolor': '#add6c8', 'edgecolor': 'none'},
         'other': {'facecolor': 'red'},
     },
@@ -128,6 +145,11 @@ tag_styles = {
         'other': {'facecolor': 'red'},
         'pole': {'markercolor': '#928f8f'},
     },
+    'railway': {
+        'abandoned': {'linecolor': 'none'},
+        'bridge-casing': {'linecolor': 'black'},
+        'station-color': {'markercolor': '#7981b0'},
+    },
     'shop': {
         'shop-icon': {'markercolor': '#ac39ac'},
         'other': {'facecolor': '#ac39ac', 'linecolor': '#ac39ac', 'markercolor': '#ac39ac'},
@@ -137,6 +159,8 @@ tag_styles = {
     },
 }
 
+tag_styles['aeroway']['aerodrome'] = tag_styles['aeroway']['airtransport']
+tag_styles['aeroway']['taxiway'] = tag_styles['aeroway']['runway']
 # generic bases like other generic bases
 tag_styles['amenity']['culture'] = tag_styles['amenity']['amenity-brown']
 tag_styles['amenity']['public-service'] = tag_styles['amenity']['amenity-brown']
@@ -144,15 +168,20 @@ tag_styles['amenity']['public-service'] = tag_styles['amenity']['amenity-brown']
 tag_styles['amenity']['art_centre'] = tag_styles['amenity']['societal_amenities']
 tag_styles['amenity']['atm'] = tag_styles['amenity']['amenity-brown']
 tag_styles['amenity']['bank'] = tag_styles['amenity']['public-service']
+tag_styles['amenity']['bbq'] = tag_styles['amenity']['gastronomy-icon']
 tag_styles['amenity']['bench'] = tag_styles['amenity']['man-made-icon']
 tag_styles['amenity']['bicycle_parking'] = tag_styles['highway']['transportation-icon']
+tag_styles['amenity']['bicycle_rental'] = tag_styles['highway']['transportation-icon']
 tag_styles['amenity']['cafe'] = tag_styles['amenity']['amenity-brown']
 tag_styles['amenity']['cemetery'] = tag_styles['landuse']['cemetery']
 tag_styles['amenity']['community_centre'] = tag_styles['amenity']['culture']
+tag_styles['amenity']['courthouse'] = tag_styles['amenity']['public-service']
+tag_styles['amenity']['drinking_water'] = tag_styles['amenity']['amenity-brown']
 tag_styles['amenity']['fast_food'] = tag_styles['amenity']['gastronomy-icon']
 tag_styles['amenity']['fuel'] = tag_styles['highway']['transportation-icon']
 tag_styles['amenity']['grave_yard'] = tag_styles['landuse']['cemetery']
 tag_styles['amenity']['hospital'] = tag_styles['amenity']['societal_amenities']
+tag_styles['amenity']['ice_cream'] = tag_styles['amenity']['gastronomy-icon']
 tag_styles['amenity']['kindergarten'] = tag_styles['amenity']['societal_amenities']
 tag_styles['amenity']['library'] = tag_styles['amenity']['culture']
 tag_styles['amenity']['pharmacy'] = tag_styles['amenity']['health-color']
@@ -161,6 +190,7 @@ tag_styles['amenity']['post_office'] = tag_styles['amenity']['public-service']
 tag_styles['amenity']['pub'] = tag_styles['amenity']['gastronomy-icon']
 tag_styles['amenity']['public-service'] = tag_styles['amenity']['amenity-brown']
 tag_styles['amenity']['recycling'] = tag_styles['amenity']['amenity-brown']
+tag_styles['amenity']['restaurant'] = tag_styles['amenity']['gastronomy-icon']
 tag_styles['amenity']['school'] = tag_styles['amenity']['societal_amenities']
 tag_styles['amenity']['shelter'] = tag_styles['amenity']['man-made-icon']
 tag_styles['amenity']['street_lamp'] = tag_styles['highway']['streetlamp']
@@ -179,6 +209,7 @@ tag_styles['highway']['bus_stop'] = tag_styles['highway']['transportation-icon']
 tag_styles['highway']['primary_link'] = tag_styles['highway']['primary']
 tag_styles['highway']['secondary_link'] = tag_styles['highway']['secondary']
 tag_styles['highway']['service'] = tag_styles['highway']['residential']
+tag_styles['highway']['steps'] = tag_styles['highway']['footway']
 tag_styles['highway']['unclassified'] = tag_styles['highway']['residential']
 
 tag_styles['landuse']['brownfield'] = tag_styles['landuse']['construction']
@@ -192,10 +223,12 @@ tag_styles['leisure']['recreation_ground'] = tag_styles['landuse']['leisure']
 tag_styles['leisure']['track'] = tag_styles['leisure']['pitch']
 
 tag_styles['natural']['bare_rock'] = tag_styles['natural']['bare_ground']
+tag_styles['natural']['bay'] = tag_styles['natural']['water']
 tag_styles['natural']['grassland'] = tag_styles['landuse']['grass']
 tag_styles['natural']['peak'] = tag_styles['natural']['landform-color']
 tag_styles['natural']['saddle'] = tag_styles['natural']['landform-color']
 tag_styles['natural']['scree'] = tag_styles['natural']['bare_ground']
+tag_styles['natural']['spring'] = tag_styles['natural']['water']
 tag_styles['natural']['wood'] = tag_styles['natural']['forest']
 #tag_styles['natural']['wetland'] = tag_styles['landuse']['grass']
 
@@ -204,11 +237,20 @@ tag_styles['place']['islet'] = tag_styles['place']['island']
 tag_styles['power']['other'] = tag_styles['landuse']['industrial']
 tag_styles['power']['tower'] = tag_styles['power']['pole']
 
+tag_styles['railway']['monorail'] = tag_styles['railway']['bridge-casing']
+tag_styles['railway']['preserved'] = tag_styles['railway']['bridge-casing']
+tag_styles['railway']['rail'] = tag_styles['railway']['bridge-casing']
+tag_styles['railway']['station'] = tag_styles['railway']['station-color']
+
 tag_styles['shop']['butcher'] = tag_styles['shop']['shop-icon']
+tag_styles['shop']['convenience'] = tag_styles['shop']['shop-icon']
+tag_styles['shop']['pet'] = tag_styles['shop']['shop-icon']
 
 # Personal added
 tag_styles['landuse']['aquaculture'] = tag_styles['landuse']['leisure']
 tag_styles['landuse']['conservation'] = tag_styles['natural']['forest']
+
+tag_styles['waterway']['riverbank'] = tag_styles['natural']['water']
 
 def _get_style(tag_styles, style_element, default_value):
     '''
@@ -236,12 +278,37 @@ def _get_style(tag_styles, style_element, default_value):
 
     return styles
 
-def carto_plot(gdf, tags=None, left=None, right=None, top=None, bottom=None):
+def carto_plot(gdf, tags=None, left=None, right=None, top=None, bottom=None, figsize=(16,16)):
+    '''
+    Crude approximation of the OSM carto stylesheet for visual comparison with osm.org
+    
+    See: https://github.com/gravitystorm/openstreetmap-carto/blob/master/style/
+    
+    leaving tags=None will plot all geometry but smaller geometries may be hidden by
+    larger ones, passing tags in in order will help to plot in that order (e.g.
+    tags={'place': True, 'landuse': True, 'building': True}) putting smaller geometries
+    on top.
+    
+    With apologies to the OpenStreetMap Carto team
+    
+    Parameters
+    ----------
+    gdf : GeoDataFrame
+        the GeoDataFrame to plot
+    figsize : tuple
+        image size in inches
+    tags : 
+        the tags to plot, None will plot all
+    left, right : numbers
+        coordinates to limit the plot area - have to be specified together
+    top, bottom : numbers
+        coordinates to limit the plot area - have to be specified together
+    '''
 
     geometry_types = [['Polygon', 'MultiPolygon'],['LineString'],['Point']]
     #tags_to_plot = ['landuse', 'power', 'amenity', 'natural', 'building', 'highway', 'waterway']
     
-    fig, ax = plt.subplots(figsize=(24,24))
+    fig, ax = plt.subplots(figsize=figsize)
     ax.set_facecolor('#f2efe9')
 
     if left and right:
@@ -317,3 +384,62 @@ def carto_plot(gdf, tags=None, left=None, right=None, top=None, bottom=None):
                     gdf[geometry_and_tag_mask].plot(ax=ax, color=markercolors_series[geometry_and_tag_mask])
                     
     return fig, ax
+
+def comparison_report(osmnx_result, overpassturbo_result, query_polygon):
+    comparison_df = pd.concat([osmnx_result.geom_type.value_counts(),
+                               overpassturbo_result.geom_type.value_counts()], axis=1)
+    comparison_df.rename(columns={0:'osmnx', 1:'overpass-turbo'}, inplace=True)
+
+    print(len(set(osmnx_result['unique_id']).intersection(set(overpassturbo_result['@id']))),
+          "ids that are in both GeoDataFrames\n"
+    )
+    
+    url = "https://www.openstreetmap.org/"
+    ids_not_in_both = set(osmnx_result['unique_id']).symmetric_difference(set(overpassturbo_result['@id']))
+    print(f"{len(ids_not_in_both)} ids that are not in both GeoDataFrames")
+    
+    print("\nids not in overpassturbo_result:\n"
+          f"{[url + _id for _id in set(osmnx_result['unique_id']).difference(set(overpassturbo_result['@id']))]}")
+    print("ids not in osmnx_result:\n"
+          f"{[url + _id for _id in set(overpassturbo_result['@id']).difference(set(osmnx_result['unique_id']))]} \n")
+
+    print(comparison_df)
+    
+    if len(ids_not_in_both)>0:
+        ax=query_polygon.plot(figsize=(8,8),
+                              zorder=0)
+        osmnx_result[osmnx_result['unique_id'].isin(ids_not_in_both)].plot(ax=ax,
+                                                                           color='red',
+                                                                           label='osmnx result',
+                                                                           legend=True,
+                                                                           zorder=1)
+        overpassturbo_result[overpassturbo_result['@id'].isin(ids_not_in_both)].plot(ax=ax,
+                                                                                     color='green',
+                                                                                     label='overpass-turbo result',
+                                                                                     legend=True,
+                                                                                     zorder=1)
+        
+        # legend
+        legend_elements = [Patch(facecolor='red',
+                                 edgecolor='red',
+                                 label='osmnx result'),
+                           Patch(facecolor='green',
+                                 edgecolor='green',
+                                 label='overpass-turbo result')
+                          ]
+        
+        # minx, miny, maxx, maxy
+        tba = osmnx_result[osmnx_result['unique_id'].isin(ids_not_in_both)].total_bounds
+        tbb = overpassturbo_result[overpassturbo_result['@id'].isin(ids_not_in_both)].total_bounds
+        tb = np.array([tba, tbb])
+        tb_max = np.nanmax(tb, axis=0)
+        tb_min = np.nanmin(tb, axis=0)
+        left = tb_min[0]
+        right = tb_max[2]
+        extra_width = (right-left) + 0.01
+        top = tb_max[3]
+        bottom = tb_min[1]
+        extra_height = (top-bottom) + 0.01
+        ax.set_xlim(left-extra_width, right+extra_width)
+        ax.set_ylim(bottom-extra_height, top+extra_height)
+        ax.legend(handles=legend_elements)
